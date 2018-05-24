4e1c5d70a16149f0a5aa2671a27ae3f8fd8888ce
nathias <nihilnegativum@protonmail.com>
2018-05-24 17:37:33 +0200

    guess a number

---
+++ days.py
@@ -0,0 +1,11 @@
+c = 50
+h = 30
+
+import math
+x = []
+y = [i for i in input('give me a number: ').split(',')]
+
+for d in y:
+    x.append(str(int(round(math.sqrt(2*c*float(d)/h)))))
+
+print(','.join(x))
---
+++ guess_random.py
@@ -0,0 +1,8 @@
+import random
+
+target_num, guess_num = random.randint(1,10), 0
+
+while target_num != guess_num:
+    guess_num = int(input('guess a number between 1 and 10'))
+
+print('game over')
